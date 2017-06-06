5e638203363dfc6cdc5b3c0802ba827db41df5c5
70n1 <el.70n1@gmail.com>
2017-06-06 12:33:18 +0200

    Selección de candidatos a círculos.

--- app/src/main/java/example/org/proyectobase/ProcesadorFiltros.java
+++ app/src/main/java/example/org/proyectobase/ProcesadorFiltros.java
@@ -3,9 +3,15 @@ package example.org.proyectobase;
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by AMARTIN on 30/05/2017.
@@ -81,11 +87,48 @@ public class ProcesadorFiltros {
         Imgproc.dilate(entrada, gray_dilation, SE ); // 3x3 dilation
         Core.subtract(gray_dilation, entrada, grad);
 
-        Mat salida = new Mat();
+
+        Mat binaria = new Mat();
         int contraste = 2;
         int tamano = 7;
-        Imgproc.adaptiveThreshold(grad, salida,255, Imgproc.ADAPTIVE_THRESH_MEAN_C,
+        Imgproc.adaptiveThreshold(grad, binaria,255, Imgproc.ADAPTIVE_THRESH_MEAN_C,
                 Imgproc.THRESH_BINARY, tamano, -contraste );
-        return  salida;
+
+
+        List<MatOfPoint> blobs = new ArrayList< MatOfPoint >() ;
+        Mat hierarchy = new Mat();
+        Mat salida = binaria.clone();//Copia porque finContours modifica entrada
+        Imgproc.cvtColor(salida, salida, Imgproc.COLOR_GRAY2RGBA);
+        Imgproc.findContours(binaria, blobs, hierarchy, Imgproc.RETR_CCOMP,
+                Imgproc.CHAIN_APPROX_NONE );
+        int minimumHeight = 30;
+        float maxratio = (float) 0.75;
+// Seleccionar candidatos a circulos
+        for (int c= 0; c< blobs.size(); c++ ) {
+            double[] data = hierarchy.get(0,c);
+            int parent = (int) data[3];
+            if(parent < 0) //Contorno exterior: rechazar
+                continue;
+            Rect BB = Imgproc.boundingRect(blobs.get(c) );
+// Comprobar tamaño
+            if ( BB.width < minimumHeight || BB.height < minimumHeight)
+                continue;
+// Comprobar anchura similar a altura
+            float wf = BB.width;
+            float hf = BB.height;
+            float ratio = wf / hf;
+            if(ratio < maxratio || ratio > 1.0/maxratio)
+                continue;
+// Comprobar no está cerca del borde
+            if(BB.x < 2 || BB.y < 2)
+                continue;
+            if(entrada.width() - (BB.x + BB.width) < 3 || entrada.height() - (BB.y + BB.height) < 3)
+                continue;
+// Aqui cumple todos los criterios. Dibujamos
+            final Point P1 = new Point(BB.x, BB.y);
+            final Point P2 = new Point(BB.x+BB.width, BB.y+BB.height);
+            Imgproc.rectangle(salida, P1, P2, new Scalar(255,0,0) );
+        } // for
+        return salida;
     }
 }
